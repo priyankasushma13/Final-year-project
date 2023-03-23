ec707ec2e6c842e50b7bc60be7b14a8d3fad4158
Sushma Priyanka <78401534+priyankasushma13@users.noreply.github.com>
2023-03-23 22:58:02 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,43 @@
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
+import pandas as pd
+import datetime
+from sklearn.tree import DecisionTreeRegressor
+
+dtr = pickle.load(open('cabfare.pkl','rb'))
+
+app = Flask(__name__,template_folder="templates")
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        data1 = request.form["day"]
+        day = int(pd.to_datetime(data1, format="%Y-%m-%dT%H:%M").day)
+        Month = int(pd.to_datetime(data1, format ="%Y-%m-%dT%H:%M").month)
+        year= int(pd.to_datetime(data1, format ="%Y-%m-%dT%H:%M").year)
+        Hour = int(pd.to_datetime(data1, format ="%Y-%m-%dT%H:%M").hour)
+        Minute = int(pd.to_datetime(data1, format ="%Y-%m-%dT%H:%M").minute)
+        data2=request.form["DayofWeek"]
+        data3=request.form['passenger_count']
+        data4=request.form['distance']
+        data=[[day,Month,year,Hour,Minute,data2,data3,data4]]
+        prediction1=dtr.predict(data)[0]
+        prediction=prediction1*80
+    return render_template('home.html',prediction="Your cab price is {} ".format(prediction))
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+@app.route('/ContactUs.html')
+def info():
+    return render_template('ContactUs.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
